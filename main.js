138de39b22c8a69840359ddba44e1b1b62b8762f
buckwheat <comp322012016@mail.ru>
2016-09-13 17:35:50 +0300

    423

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ c='2,1';
 console.log(a);
 console.log(b);
 console.log(c);
+
+if (null == undefined)
+{
+	console.log('onirovni');
+}
+else
+{
+	console.log('oninerovni');
+}
+
+var s;
+s="текст47";
+s=Number(s);
+console.log(s);
+s=Boolean(s);
+console.log(s);
